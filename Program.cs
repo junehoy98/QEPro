11abef5682e8f76a4fbdba3ee6ee115bd473d55d
junehoy98 <junehoy@umich.edu>
2022-09-19 18:40:15 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSpectrometers;
+            //int spectrometerIndex;
+            OmniDriver.NETWrapper wrapper = new OmniDriver.NETWrapper();
+            numberOfSpectrometers = wrapper.openAllSpectrometers();
+            Console.WriteLine($"number = {numberOfSpectrometers}");
+            return;
+        }
+    }   
+}
